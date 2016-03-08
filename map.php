1279b9e6beec4fac93f2ec0766c98db715c3e333
iainscott-rgu <odyssey3007@hotmail.com>
2016-03-08 12:37:21 +0000

    0

--- map.php
+++ map.php
@@ -80,7 +80,7 @@
 </head>
 
 <body onload="load()">
-<div id="map" style="width: 500px; height: 300px"></div>
+<div id="map" style="width:500px;height:500px;background:snow"></div>
 </body>
 
 </html>
