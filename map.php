c3923be6048abb1a2b3b6cdec735fc4ec91eddef
iainscott-rgu <odyssey3007@hotmail.com>
2016-03-08 10:27:32 +0000

    0

--- map.php
+++ map.php
@@ -24,12 +24,12 @@ var markers = xml.documentElement.getElementsByTagName("marker");
 for (var i = 0; i < markers.length; i++) {
 var name = markers[i].getAttribute("name");
 var address = markers[i].getAttribute("address");
-var type = markers[i].getAttribute("type");
+
 var point = new google.maps.LatLng(
 parseFloat(markers[i].getAttribute("lat")),
 parseFloat(markers[i].getAttribute("lng")));
 var html = "<b>" + name + "</b> <br/>" + address;
-var icon = customIcons[type] || {};
+
 var marker = new google.maps.Marker({
 map: map,
 position: point,
